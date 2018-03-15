940e6a645a700b918235ad6e18e54e252558155a
Gemma González Gil <ggg-95@hotmail.com>
2018-03-15 11:25:41 +0100

    Mapeado user

--- src/main/java/inciDashboard/entities/User.java
+++ src/main/java/inciDashboard/entities/User.java
@@ -1,16 +1,28 @@
 package inciDashboard.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@Entity
 public class User {
 
 	private static final Logger LOG = LoggerFactory.getLogger(User.class);
 
+	@Id
+	@GeneratedValue
 	private Long id;
 	private String name;
+	@Column(unique = true)
 	private String email;
 	private String password;
+
+	public User() {
+	}
 
 	public User(String name, String email, String password) {
 		LOG.info("Creating user " + name + ". Email: " + email);
@@ -40,9 +52,6 @@ public class User {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((email == null) ? 0 : email.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((password == null) ? 0 : password.hashCode());
 		return result;
 	}
 
@@ -59,21 +68,6 @@ public class User {
 			if (other.email != null)
 				return false;
 		} else if (!email.equals(other.email))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (password == null) {
-			if (other.password != null)
-				return false;
-		} else if (!password.equals(other.password))
 			return false;
 		return true;
 	}
