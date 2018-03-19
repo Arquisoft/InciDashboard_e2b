33e5ebc3cf605097399b7e8aa352593c14cb95ac
Gemma González Gil <UO236976@uniovi.es>
2018-03-19 13:08:20 +0100

    Creada entidad incidencia

---
+++ src/main/java/inciDashboard/entities/Incidencia.java
@@ -0,0 +1,95 @@
+package inciDashboard.entities;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Incidencia {
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String titulo;
+	@Temporal(value = TemporalType.DATE)
+	private Date fecha;
+	private String descripcion;
+
+	public Incidencia() {
+	}
+
+	public Incidencia(Date fecha, String descripcion) {
+		super();
+		this.fecha = fecha;
+		this.descripcion = descripcion;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((fecha == null) ? 0 : fecha.hashCode());
+		result = prime * result + ((titulo == null) ? 0 : titulo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Incidencia other = (Incidencia) obj;
+		if (fecha == null) {
+			if (other.fecha != null)
+				return false;
+		} else if (!fecha.equals(other.fecha))
+			return false;
+		if (titulo == null) {
+			if (other.titulo != null)
+				return false;
+		} else if (!titulo.equals(other.titulo))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Incidencia [id=" + id + ", titulo=" + titulo + ", fecha=" + fecha + ", descripcion=" + descripcion
+				+ "]";
+	}
+
+}
