d70716c03e22483593240102f817976030b60a13
Pelayo Martinez <pmartinezcapela@gmail.com>
2018-03-19 13:15:08 +0100

    Añadido kafka y .bat de arranque
    Añadida la carpeta con zookeeper y kafka y un .bat que ejecuta ambos servicios

---
+++ bootKafka.bat
@@ -0,0 +1,5 @@
+cd kafka_2.11-1.0.1\bin\windows
+rmdir /s /q C:\tmp\kafka-logs
+start cmd /k zookeeper-server-start.bat ..\..\config\zookeeper.properties
+timeout /t 10
+kafka-server-start.bat ..\..\config\server.properties
