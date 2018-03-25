304ea909f9820280a1aa60a88be973f2fb60cef2
Pablo Hevia <pablohv12@gmail.com>
2018-03-26 01:19:00 +0200

    Añadidas etiquetas a las incidencias

--- src/main/java/inciDashboard/entities/Incidencia.java
+++ src/main/java/inciDashboard/entities/Incidencia.java
@@ -1,151 +1,174 @@
 package inciDashboard.entities;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapKeyColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 public class Incidencia {
-    @Id
-    @GeneratedValue
-    private Long id;
-    private String nombreUsuario;
-    private String nombre;
-    private String descripcion;
-    @OneToOne
-    @JoinColumn(name = "coordenadas_id")
-    private Coordenadas coordenadas;
-    private InciStatus estado;
-    
-    @OneToMany(mappedBy = "incidencia", cascade = CascadeType.ALL)
-    private Set<Comentario> comentarios = new HashSet<Comentario>();
-    
-    @Temporal(value = TemporalType.DATE)
-    private Date caducidad;
-    
-    
-    @ElementCollection(fetch=FetchType.EAGER)
-    @CollectionTable(name = "CAMPOS")
-    @MapKeyColumn(name = "KEY")
-    @Column(name = "VALUE")
-    private Map<String,String> campos = new HashMap<String, String>(); //Resto de valores que pueden variar dependiendo de la incidencia
-    
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String nombreUsuario;
+	private String nombre;
+	private String descripcion;
+	@OneToOne
+	@JoinColumn(name = "coordenadas_id")
+	private Coordenadas coordenadas;
+	private InciStatus estado;
 
-    private boolean danger = false;
+	@OneToMany(mappedBy = "incidencia", cascade = CascadeType.ALL)
+	private Set<Comentario> comentarios = new HashSet<Comentario>();
 
-    public Incidencia() {
-    }
+	@Temporal(value = TemporalType.DATE)
+	private Date caducidad;
 
-    public Incidencia(String nombreUsuario, String nombre, String descripcion, Coordenadas coordenadas, Date caducidad, User user, Map<String, String> campos) {
-	    super();
-        this.nombreUsuario = nombreUsuario;
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.coordenadas = coordenadas;
-        this.estado = InciStatus.ABIERTA;
-        this.caducidad = caducidad;
-        this.user = user;
-        this.campos = campos;
-        checkDangerousness();
-    }
+	@ElementCollection(fetch = FetchType.EAGER)
+	@CollectionTable(name = "CAMPOS")
+	@MapKeyColumn(name = "KEY")
+	@Column(name = "VALUE")
+	private Map<String, String> campos = new HashMap<String, String>(); // Resto de valores que pueden variar
+																		// dependiendo de la incidencia
 
-    private void checkDangerousness() {
-        for (Map.Entry<String, String> entry : campos.entrySet()) {
-            if (entry.getKey().equals("temp")) {
-                int temp = Integer.valueOf(entry.getValue()); {
-                    if (temp > 40 || temp < 1) {
-                        danger = true;
-                        break;
-                    }
-                }
-            }
-            else if (entry.getKey().equals("wspeed")) {
-                int speed = Integer.valueOf(entry.getValue()); {
-                    if (speed > 60 || speed < 5) {
-                        danger = true;
-                        break;
-                    }
-                }
-            }
-        }
-    }
+	@ElementCollection
+	@CollectionTable(name = "tags")
+	private List<String> etiquetas = new ArrayList<String>();
 
-    public boolean isDanger() {
-        return danger;
-    }
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
 
-    public String getNombreUsuario() {
-	return nombreUsuario;
-    }
+	private boolean danger = false;
 
-    public void setNombreUsuario(String nombreUsuario) {
-	this.nombreUsuario = nombreUsuario;
-    }
+	public Incidencia() {
+	}
 
-    public String getNombre() {
-	return nombre;
-    }
+	public Incidencia(String nombreUsuario, String nombre, String descripcion, Coordenadas coordenadas, Date caducidad,
+			User user, Map<String, String> campos, List<String> etiquetas) {
+		super();
+		this.nombreUsuario = nombreUsuario;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.coordenadas = coordenadas;
+		this.estado = InciStatus.ABIERTA;
+		this.caducidad = caducidad;
+		this.user = user;
+		this.campos = campos;
+		this.etiquetas = etiquetas;
+		checkDangerousness();
+	}
 
-    public void setNombre(String nombre) {
-	this.nombre = nombre;
-    }
+	private void checkDangerousness() {
+		for (Map.Entry<String, String> entry : campos.entrySet()) {
+			if (entry.getKey().equals("temp")) {
+				int temp = Integer.valueOf(entry.getValue());
+				{
+					if (temp > 40 || temp < 1) {
+						danger = true;
+						break;
+					}
+				}
+			} else if (entry.getKey().equals("wspeed")) {
+				int speed = Integer.valueOf(entry.getValue());
+				{
+					if (speed > 60 || speed < 5) {
+						danger = true;
+						break;
+					}
+				}
+			}
+		}
+	}
 
-    public String getDescripcion() {
-	return descripcion;
-    }
+	public boolean isDanger() {
+		return danger;
+	}
 
-    public void setDescripcion(String descripcion) {
-	this.descripcion = descripcion;
-    }
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
 
-    public Coordenadas getCoordenadas() {
-	return coordenadas;
-    }
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
 
-    public void setCoordenadas(Coordenadas coordenadas) {
-	this.coordenadas = coordenadas;
-    }
+	public String getNombre() {
+		return nombre;
+	}
 
-    public Long getId() {
-	return id;
-    }
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
-    public InciStatus getEstado() {
-	return estado;
-    }
+	public String getDescripcion() {
+		return descripcion;
+	}
 
-    public void setEstado(InciStatus estado) {
-	this.estado = estado;
-    }
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
 
-    public Set<Comentario> getComentarios() {
-	return new HashSet<Comentario>(comentarios);
-    }
+	public Coordenadas getCoordenadas() {
+		return coordenadas;
+	}
 
-    public void setComentarios(Set<Comentario> comentarios) {
-	this.comentarios = comentarios;
-    }
+	public void setCoordenadas(Coordenadas coordenadas) {
+		this.coordenadas = coordenadas;
+	}
 
-    public Date getCaducidad() {
-	return caducidad;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setCaducidad(Date caducidad) {
-	this.caducidad = caducidad;
-    }
+	public InciStatus getEstado() {
+		return estado;
+	}
 
-    public User getUser() {
-	return user;
-    }
-    
-    public Map<String, String> getCampos() {
+	public void setEstado(InciStatus estado) {
+		this.estado = estado;
+	}
+
+	public Set<Comentario> getComentarios() {
+		return new HashSet<Comentario>(comentarios);
+	}
+
+	public void setComentarios(Set<Comentario> comentarios) {
+		this.comentarios = comentarios;
+	}
+
+	public Date getCaducidad() {
+		return caducidad;
+	}
+
+	public void setCaducidad(Date caducidad) {
+		this.caducidad = caducidad;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public Map<String, String> getCampos() {
 		return new HashMap<String, String>(campos);
 	}
 
@@ -153,45 +176,49 @@ public class Incidencia {
 		this.campos = campos;
 	}
 
+	public List<String> getEtiquetas() {
+		return etiquetas;
+	}
+
 	@Override
-    public int hashCode() {
-	final int prime = 31;
-	int result = 1;
-	result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
-	result = prime * result + ((nombreUsuario == null) ? 0 : nombreUsuario.hashCode());
-	return result;
-    }
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		result = prime * result + ((nombreUsuario == null) ? 0 : nombreUsuario.hashCode());
+		return result;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-	if (this == obj)
-	    return true;
-	if (obj == null)
-	    return false;
-	if (getClass() != obj.getClass())
-	    return false;
-	Incidencia other = (Incidencia) obj;
-	if (nombre == null) {
-	    if (other.nombre != null)
-		return false;
-	} else if (!nombre.equals(other.nombre))
-	    return false;
-	if (nombreUsuario == null) {
-	    if (other.nombreUsuario != null)
-		return false;
-	} else if (!nombreUsuario.equals(other.nombreUsuario))
-	    return false;
-	return true;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Incidencia other = (Incidencia) obj;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		if (nombreUsuario == null) {
+			if (other.nombreUsuario != null)
+				return false;
+		} else if (!nombreUsuario.equals(other.nombreUsuario))
+			return false;
+		return true;
+	}
 
-    @Override
-    public String toString() {
-    	String toString = "Incidencia [id=" + id + ", nombreUsuario=" + nombreUsuario + ", nombre=" + nombre + ", descripcion="
-    			+ descripcion + ", latitud =" + coordenadas.getLatitud() + ", longitud =" + coordenadas.getLongitud() + ", estado=" + estado.toString() 
-    			+ ", caducidad=" + caducidad.toString();
-    	for(Comentario c: comentarios)
-    		toString += ", comentario=" + c.getTexto();
-    	return toString + " ]";
-    }
+	@Override
+	public String toString() {
+		String toString = "Incidencia [id=" + id + ", nombreUsuario=" + nombreUsuario + ", nombre=" + nombre
+				+ ", descripcion=" + descripcion + ", latitud =" + coordenadas.getLatitud() + ", longitud ="
+				+ coordenadas.getLongitud() + ", estado=" + estado.toString() + ", caducidad=" + caducidad.toString();
+		for (Comentario c : comentarios)
+			toString += ", comentario=" + c.getTexto();
+		return toString + " ]";
+	}
 
 }
